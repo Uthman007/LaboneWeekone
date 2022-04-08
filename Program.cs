367232325e98435d674ee097047ef58c46eed972
Adewuyi uthman alao <adewuyiuthmanalao@gmail.com>
2022-04-09 00:00:31 +0100

    third commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LaboneWeekone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            string myName = " My name is Azeezah Musa";
+            string myTrack = " Backend Track,";
+            string myReason = " my goal is to become a backend developer in six month.";
+
+            
+            Console.Write("Hello");
+            Console.Write(myName);
+            Console.Write(" I am in");
+            Console.Write(myTrack);
+            Console.WriteLine(myReason);
+        }
+    }
+}
